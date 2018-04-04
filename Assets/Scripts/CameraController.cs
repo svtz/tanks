4612065ddd5fc978586be00e841d7e9d065ab474
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2018-04-04 16:47:05 +0300

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + _offset;
+    }
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D _rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        var movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        _rb2d.AddForce(movement * Speed);
+    }
+}
