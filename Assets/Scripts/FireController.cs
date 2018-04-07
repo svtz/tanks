a3e34e81cfb84a49f799ba20ea9a7d92dcf11d7a
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2018-04-07 22:15:38 +0300

    снарядик!

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour {
+
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        Fire();
+	    }
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            BulletPrefab,
+            BulletSpawn.position,
+            BulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
