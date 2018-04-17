e1ab7ae9276edf893b960cf14f3acaea749aa87a
Grigory Kibanov <gkibanov@mail.ru>
2018-04-17 21:11:00 +0300

    Зачатки лобби и меню

---
+++ Assets/Scripts/Network/CustomNetworkDiscovery.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkManager))]
+
+public class CustomNetworkDiscovery : NetworkDiscovery {
+    public string serverName;
+    public string playerName;
+    public int networkPort = 7777;
+    private ServerData serverData ;
+    private NetworkManager manager;
+    public List<ServerData> findedServers = null;
+
+    void Start()
+    {
+        LogFilter.currentLogLevel = 1;
+           playerName = System.Environment.MachineName;
+        serverName = System.Environment.MachineName;
+        manager = GetComponent<NetworkManager>();
+    }
+    
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        ServerData newServerData = ScriptableObject.CreateInstance<ServerData>();
+        newServerData.Load(data);
+        OnDiscovery(newServerData);
+        Debug.Log(data);
+    }
+
+    private void OnDiscovery(ServerData newServerData)
+    {
+        for (int index = 0; index < findedServers.Count; ++index)
+        {
+            if (findedServers[index].NetworkAddress == newServerData.NetworkAddress &&
+                findedServers[index].Port == newServerData.Port)
+            {
+                findedServers[index] = newServerData;
+                return;
+            }
+        }
+        findedServers.Add(newServerData);
+    }
+
+    public bool CustomStartServer()
+    {
+        manager.StartHost();
+        if (manager.isNetworkActive && Initialize())
+        {
+            serverData = ScriptableObject.CreateInstance<ServerData>();
+            serverData.ServerName = serverName;
+            serverData.NetworkAddress = Network.player.ipAddress;
+            serverData.Port = networkPort;
+            broadcastData = serverData.ToString();
+            return StartAsServer();
+        }
+        return false;
+    }
+    public bool CustomStartClient(ServerData serverData)
+    {
+        CustomStopServerDiscovery();
+        manager.networkAddress = serverData.NetworkAddress;
+        manager.networkPort = serverData.Port;
+        manager.StartClient();
+        return manager.isNetworkActive;
+    }
+
+    public bool CustomStartServerDiscovery()
+    {
+        if (Initialize())
+        {
+            findedServers = new List<ServerData>();
+            return StartAsClient();
+        }
+        else
+            return false;
+    }
+
+    public void CustomStopServerDiscovery()
+    {
+        StopBroadcast();
+    }
+
+    public void CustomStopServer()
+    {
+        StopBroadcast();
+        manager.StopClient();
+        manager.StopHost();
+    }
+
+    public void CustomStopClient()
+    {
+        manager.StopClient();
+    }
+
+}
---
+++ Assets/Scripts/Network/CustomNetworkManager.cs
@@ -0,0 +1,59 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+public class CustomNetworkManager : NetworkLobbyManager
+{
+    public override NetworkClient StartHost()
+    {
+        return base.StartHost();
+    }
+    
+    public override bool OnLobbyServerSceneLoadedForPlayer(GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        Debug.Log("Loaded");
+        return base.OnLobbyServerSceneLoadedForPlayer(lobbyPlayer, gamePlayer);
+    }
+
+    public override void OnLobbyServerPlayersReady()
+    {
+        GetComponent<CustomNetworkDiscovery>().StopBroadcast();
+        base.OnLobbyServerPlayersReady();
+    }
+    public override GameObject OnLobbyServerCreateGamePlayer(NetworkConnection conn, short playerControllerId)
+    {
+        Debug.Log("Instantiated");
+        GameObject player = (GameObject)Instantiate(gamePlayerPrefab, Vector3.zero, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        return player;
+    }
+    /*
+    public override void ServerChangeScene(string sceneName)
+    {
+        if (sceneName == lobbyScene)
+        {
+            foreach (var lobbyPlayer in lobbySlots)
+            {
+                if (lobbyPlayer == null)
+                    continue;
+
+                // find the game-player object for this connection, and destroy it
+                var uv = lobbyPlayer.GetComponent<NetworkIdentity>();
+
+                PlayerController playerController;
+                if (uv.connectionToClient.GetPlayerController(uv.playerControllerId, out playerController))
+                {
+                    NetworkServer.Destroy(playerController.gameObject);
+                }
+
+                if (NetworkServer.active)
+                {
+                    // re-add the lobby object
+                    lobbyPlayer.GetComponent<NetworkLobbyPlayer>().readyToBegin = false;
+                    NetworkServer.ReplacePlayerForConnection(uv.connectionToClient, lobbyPlayer.gameObject, uv.playerControllerId);
+                }
+            }
+        }
+        base.ServerChangeScene(sceneName);
+    }*/
+
+}
