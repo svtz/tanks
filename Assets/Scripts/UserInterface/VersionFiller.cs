bc1fbd4272951b684054c00972ecd26393cd0600
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2018-05-03 20:00:43 +0300

    номер версии в меню, лого конфигуратора, занулил текстуры для spqcer-панелей fixes #39

---
+++ Assets/Scripts/UserInterface/VersionFiller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace svtz.Tanks.UserInterface
+{
+    public class VersionFiller : MonoBehaviour
+    {
+        private void Start ()
+        {
+            GetComponent<Text>().text = string.Format("версия {0}", Application.version);
+            Destroy(this);
+        }
+    }
+}
