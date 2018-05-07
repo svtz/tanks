b07e73c70e4bfbbdc90835d0455de77bd220cbb9
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2018-05-07 21:55:59 +0300

    Новая карта - поле. Попутно включил выравнивание камеры по пикселям.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -10,10 +10,12 @@ namespace svtz.Tanks.Camera
 #pragma warning restore 0649
 
         private GameObject _followObject;
+        private PerfectPixel _perfectPixel;
 
         public void BindTo(GameObject obj)
         {
             _followObject = obj;
+            _perfectPixel = GetComponent<PerfectPixel>();
         }
 
         public void Unbind()
@@ -29,7 +31,19 @@ namespace svtz.Tanks.Camera
             var targetPosition = _followObject.transform.position + Offset;
             var targetMove = targetPosition - transform.position;
             var currentMove = Vector3.ClampMagnitude(targetMove, MaxCameraSpeed * Time.deltaTime);
-            transform.position += currentMove;
+            var newPostion = transform.position + currentMove;
+
+            var roundedX = RoundToNearestPixel(newPostion.x);
+            var roundedY = RoundToNearestPixel(newPostion.y);
+            transform.position = new Vector3(roundedX, roundedY) + Offset;
+        }
+
+        private float RoundToNearestPixel(float unityUnits)
+        {
+            var valueInPixels = unityUnits * _perfectPixel.PPU;
+            valueInPixels = Mathf.Round(valueInPixels);
+            var roundedUnityUnits = valueInPixels * (1 / _perfectPixel.PPU);
+            return roundedUnityUnits;
         }
     }
 }
