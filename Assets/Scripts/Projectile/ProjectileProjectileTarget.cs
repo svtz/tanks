c371480ee8f15bc28b2e42b5f364908a342587f8
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2018-05-07 10:36:33 +0300

    починил взаимное уничтожение снарядов

---
+++ Assets/Scripts/Projectile/ProjectileProjectileTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace svtz.Tanks.Projectile
+{
+    internal sealed class ProjectileProjectileTarget : AbstractProjectileTarget
+    {
+        private Projectile _projectile;
+
+
+        private void Start()
+        {
+            _projectile = GetComponent<Projectile>();
+        }
+
+        public override void TakeDamage(int amount, GameObject damager)
+        {
+            _projectile.TryDespawn();
+        }
+    }
+}
